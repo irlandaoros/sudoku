9adac1e707401b8ea46d78e96d8988dc1f8c8685
Irlanda <165083041+irlandaoros@users.noreply.github.com>
2024-04-09 06:52:40 -0600

    Update __actions__.py

--- sudoku_folder/__actions__.py
+++ sudoku_folder/__actions__.py
@@ -1,9 +1,9 @@
 def add_number_to_cell(number,position,grid):
     pass
-def change_number(number,position,grid)
+def change_number(number,position,grid):
     pass
-def erase_number(eraser,number,position,grid)
+def erase_number(eraser,number,position,grid):
     pass
-def color_number(color,position)
+def color_number(color,position):
     pass
 
