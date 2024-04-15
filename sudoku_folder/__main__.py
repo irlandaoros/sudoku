e772a07113d9f7e5cad259086976ee2776446d20
Irlanda <165083041+irlandaoros@users.noreply.github.com>
2024-04-15 11:27:15 -0600

    Update __main__.py

--- sudoku_folder/__main__.py
+++ sudoku_folder/__main__.py
@@ -8,6 +8,25 @@ while True:
     position=None
     grid=None
     add_number_to_cell (number,position,grid)
+  elif command == "change number":
+    number=None
+    position=None
+    grid=None
+    change_number (number,position,grid)
+  elif command == "erase number":
+    eraser=None
+    number=None
+    position=None
+    grid=None 
+    erase_number (eraser,number,position,grid)
+  elif command == "color number":
+    color=None
+    position=None
+    color_number (color,position)
+  elif command == "if win":
+    change board=None 
+  else:
+    print("I did not understand that command")
   
   
  
