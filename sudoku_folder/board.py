6c60924a077679460922bd86b54ba6059dea3a4c
Irlanda <165083041+irlandaoros@users.noreply.github.com>
2024-04-15 11:30:09 -0600

    Update board.py

--- sudoku_folder/board.py
+++ sudoku_folder/board.py
@@ -1,9 +1,9 @@
-def print_board(numbers_and_spaces):
-  print ("print_board")
+def print_board (numbers_and_spaces):
+  print ("print board")
 def erase(eliminate_numbers,chose):
   print ("erase")
 def create_new_board():
-  print (create_new_board")
-def reset_game(new_board):
-  print ("reset_game")
+  print (create new board")
+def reset_game (new_board):
+  print ("reset game")
   
