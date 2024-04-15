fed3007f208937a021aa1283558ed767d54849ec
Irlanda <165083041+irlandaoros@users.noreply.github.com>
2024-04-15 11:27:35 -0600

    Update __main__.py

--- sudoku_folder/__main__.py
+++ sudoku_folder/__main__.py
@@ -1,3 +1,10 @@
+from actions import add_number_to_cell 
+from board import print_board 
+from actions import erase_number
+from actions import color_number 
+from board import erase 
+from board import print_new_board 
+
 with open('README.md', 'r') as f:
   print (f.read())
   
