b696ede6a0ae720986163d6b44c80dbb6c03e0cc
Irlanda <165083041+irlandaoros@users.noreply.github.com>
2024-04-15 11:15:16 -0600

    Update __main__.py

--- sudoku_folder/__main__.py
+++ sudoku_folder/__main__.py
@@ -1,3 +1,13 @@
 with open('README.md', 'r') as f:
   print (f.read())
   
+while True:
+  command = input("Enter a command: ")
+  if command == "add number to cell":
+    number=None 
+    position=None
+    grid=None
+    add_number_to_cell (number,position,grid)
+  
+  
+ 
