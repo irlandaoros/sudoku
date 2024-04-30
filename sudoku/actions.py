8dffd916ec3b429c79dc1c9996813c2a90158837
bernyqr7 <165336443+bernyqr7@users.noreply.github.com>
2024-04-30 08:12:40 -0400

    Update actions.py

--- sudoku/actions.py
+++ sudoku/actions.py
@@ -4,7 +4,5 @@ def change_number(number,position,grid):
     print ("change number")
 def erase_number(eraser,number,position,grid):
     print ("erase number")
-def color_number(color,position):
-    print ("color number")
 def if_win (change board):
     print ("if_win")
